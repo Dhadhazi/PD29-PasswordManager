10083140aba6697fb381664ad39815aaefa46dd8
Dhadhazi <dhadhazi@gmail.com>
2020-11-30 10:17:04 +0100

    Project finished

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 window = Tk()
 window.title("Password Manager")
@@ -9,6 +11,28 @@ canvas = Canvas(width=200, height=200)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    return "".join(password_list)
+
+
+def add_generated_password():
+    password_input.delete(0, END)
+    password = generate_password()
+    pyperclip.copy(password)
+    password_input.insert(0, password)
 
 
 def clear_input_fields():
@@ -49,7 +73,7 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=add_generated_password)
 generate_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, command=save_input)
 add_button.grid(column=1, row=4, columnspan=2)
